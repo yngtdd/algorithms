34a84b911025aab52f454ce08fc160825ac60724
yngtdd <yngtdd@gmail.com>
2023-02-03 15:44:13 -0500

    docs: Add documentation for contains duplicate

--- array/src/contains_duplicate.rs
+++ array/src/contains_duplicate.rs
@@ -1,5 +1,15 @@
+//! Given an integer array nums, return true if any value appears 
+//! at least twice in the array, and return false if every element is distinct.
 use std::collections::HashSet;
 
+/// Check if an array contains duplicates
+///
+/// We can create a set of elements and compare whether
+/// that set has fewer elements than the original array
+///
+/// # Note
+///
+/// Uses O(N) memory
 fn contains_duplicate(nums: Vec<i32>) -> bool {
     nums.iter().collect::<HashSet<_>>().len() < nums.len()
 }
